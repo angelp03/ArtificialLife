4fc51080d351cba78351046751d4f6f7c1005d13
angelp03 <angelpimentel2025@u.northwestern.edu>
2024-01-16 13:30:02 -0600

    aesthetic and simulation improvements

--- HW1/handSimulation.py
+++ HW1/handSimulation.py
@@ -2,19 +2,21 @@ import mujoco
 import mujoco_viewer
 import numpy as np
 
-model = mujoco.MjModel.from_xml_path('/Users/angelp/Desktop/ArtificialLife/HW1/legmovement.xml')
+model = mujoco.MjModel.from_xml_path('/Users/angelp/Desktop/ArtificialLife/HW1/simpleHand.xml')
 data = mujoco.MjData(model)
 
 
 viewer = mujoco_viewer.MujocoViewer(model, data)
 actuators = model.nu
+#array of velocities
 velocities = np.array ([800, 300, 800, 300, 800, 300, 800, 300])
 
 for i in range(10000):
     if viewer.is_alive:
-        if i % 500 == 0: # invert velocities every 500 steps.
+        # when all joints have reached terminal velocity, invert
+        if np.all(np.abs(data.qvel) < 0.01):
+            data.ctrl[:actuators] = velocities
             velocities *= -1
-        data.ctrl[:actuators] = velocities
         mujoco.mj_step(model, data)
         viewer.render()
     else:
