3d7dfc3ac788e54674c0bbcb58394034c32cccb0
Angel Pimentel <angelpimentel2025@u.northwestern.edu>
2024-02-14 23:10:06 -0600

    adding templates and beginning initial population generation

--- HW4/basicMutation.py
+++ HW4/basicMutation.py
@@ -2,18 +2,72 @@ import numpy as np
 import json
 import os
 
+
+xmlTemplate = """
+<mujoco>
+    <option gravity = '0 0 -9.81'/>
+    <worldbody>
+        <light diffuse = '.5 .5 .5' pos = '0 0 6' dir = '0 0 -1'/>
+        <geom type = 'plane' size = '5 5 0.1' rgba = '1 1 1 1'/>
+        <body name = '0' pos = '0 0 1'>
+            <joint type = 'free'/>
+            {} 
+        </body>
+    </worldbody>
+    <actuator>
+        {}
+    </actuator>
+</mujoco>
+"""
+
+motorTemplate = """<motor name = {} joint ={}/> 
+                    {}"""
+
+bodyTemplate = """
+        <body name = '{}' pos='{}' euler='{}'> 
+            {}
+        </body>
+        {}"""
+
+geomTemplate = """<geom type='{}' size='{}'/>"""
+
+jointTemplate = """<joint type='{}' name='{}' axis='{}' range='{}'/>"""
+
+bodyTypes = ["sphere", "box"]
+geomTypes = ["box", "capsule", "cylinder"]
+
+def generateGeom (type: str):
+    if type == "sphere":
+        return geomTemplate.format(type, f"{1}")
+    elif type == "cylinder" or type == "capsule":
+        return geomTemplate.format(type, f"{1} {1}")
+    elif type == "box":
+        return geomTemplate.format(type, f"{1} {1} {1}")
+
+
 # Make a simulation class in order to generate, store, and load based on unique seed
 class Simulation:
     # Set initial values based on seed
     def __init__(self, seed=None) -> None:
         self.seed = seed
         self.generation = 0
-        self.population = None
+        self.population = []
+    
     # Create the first "parent" models, this is only done upon the creation of a simulation
     def initialize_population(self):
         if self.seed is not None:
             # Set unique seed for population
             np.random.seed(self.seed)
+            for model in range(np.random.randint(4,7)): # Generate random number of starting parents
+                body = generateGeom(np.random.choice(bodyTypes))
+                limbs = ''
+                for limb in range(np.random.randint(2,5)): # Generate random number of limb
+                    limbs += bodyTemplate #Add body for limb
+                    limbs.format("name", f"{0} {0} {0}",generateGeom(np.random.choice(geomTypes)), "")
+                body += bodyTemplate.format("name", f"{0} {0} {0}", f"{0} {0} {90}", limbs, "")
+
+
+
         pass
     # This can be done within the simulation class or can create a seperate function that takes simulation class as input
     def run_generation(self):
